c0650461cbacbc82c5e55fbdedc419216f90a870
Xuelei Dai <daixuelei2014@163.com>
2023-03-29 20:00:06 +0800

    Delete 02.multi-assembly_graph_construction directory

--- 02.multi-assembly_graph_construction/Minigraph_Cactus_graph/RNAmap/vg.rna.sh
+++
@@ -1,5 +0,0 @@
-#Create a spliced pangenome graph and indexes
-vg autoindex --workflow mpmap -t 4 --prefix vg_rna --ref-fasta ARS.fa --vcf cattle_genotyping.vcf.gz --tx-gff GCF_002263795.1_ARS-UCD1.2_genomic.ChangeChromName.Chromosome.gtf
-#RNA-seq reads can be mapped to the spliced pangenome graph using vg mpmap
-vg mpmap -n rna -t 4 -x vg_rna.spliced.xg -g vg_rna.spliced.gcsa -d vg_rna.spliced.dist -f data/ERR789826_1.clean.fq.gz -f data/ERR789826_2.clean.fq.gz > mpmap.gamp
-
--- 02.multi-assembly_graph_construction/Minigraph_Cactus_graph/genotyping/Genotype.pangenie.sh
+++
@@ -1,6 +0,0 @@
-#This removes all non-top-level bubbles from the VCF, unless they are nested inside a top-level bubble with a reference length exceeding 100kb.
-vcfbub -l 0 -r 100000 -i ../cattle.pg/cattle.pg.vcf.gz > cattle.pg.filter.vcf.gz
-#This removes all records carrying a missing allele: “.” or “CONFLICT”.
-cat cattle.pg.filter.vcf|awk '$10!=".|."'|awk '$10!=".|1"'|awk '$10!="1|."'|awk '$10!="2|2"'|grep -v "CONFLICT=" >cattle.pg.filter.rm.vcf
-#Genotyping and phasing based on kmer-counting and known haplotype sequences.
-PanGenie -i ../data/SRR12452240.fastq -r ../data/ARS.fa -v cattle.pg.filter.rm.vcf -o cattle
